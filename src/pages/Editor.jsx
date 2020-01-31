55dde40a65a21e63b96b205fd77571b8b3491102
Troy Chryssos <troychryssos@gmail.com>
2020-01-31 17:02:40 -0500

    much cooler initial text

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -45,6 +45,61 @@ const useStyles = createUseStyles({
 	},
 })
 
+const initialState = [
+	{
+		type: 'paragraph',
+		children: [{ text: 'Now in this time of diaspora there was a war between AURYX and SAVATHÛN and XIVU ARATH.' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: '' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: 'Brother Auryx, said SAVATHÛN, do not forgive my betrayal. Instead, take vengeance upon me for what I did at the dry moon! And AURYX made war on her, in worship of the Deep. Between them stood XIVU ARATH saying, stop, or I will kill you, war is mine and I am strongest.' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: '' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: 'This was how they worshipped.' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: '' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: 'For twenty thousand years they fought across the moons and they fought in the abyssal plains and lightning palaces of each other\'s sword spaces. And they killed each other again and again, so that they could practice death.' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: '' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: 'Such was their love.' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: '' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: 'At last the many moons came to many worlds and it was time to go to war on life. AURYX said, I shall establish a court, and whoever comes into this court may challenge me. My court will be the High War. It will be a killing ground and a school of the sword logic we have learned from our gods.' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: '' }],
+	},
+	{
+		type: 'paragraph',
+		children: [{ text: 'SAVATHÛN thought this was a great idea. She made a court called the High Coven. XIVU ARATH said, the world is my court, wherever there is war.' }],
+	},
+]
+
 // START - ELEMENTS - START
 const CodeElement = ({ attributes, children }) => (
 	<pre {...attributes}>
@@ -140,18 +195,12 @@ const FormattingButton = ({
 		<span className={labelClassName}>{label}</span>
 	</button>
 )
-
 // END - FORMATTING BUTTONS - END
 
 export default () => {
 	// START - DEFINITIONS - START
 	const classes = useStyles()
-	const [value, setValue] = useState([
-		{
-			type: 'paragraph',
-			children: [{ text: 'A line of text in a paragraph.' }],
-		},
-	])
+	const [value, setValue] = useState(initialState)
 	const editor = useMemo(() => withReact(createEditor()), [])
 	// END - DEFINITIONS - END
 
